3a4e3ba4e52cf4f4d80656462134859c11984b77
細野拓斗 <hosono@plucre.com>
2024-03-09 15:01:30 +0900

    Update : 色と図形の描画

--- book_2/SDL_TEST01/include/game.hpp
+++ book_2/SDL_TEST01/include/game.hpp
@@ -5,20 +5,38 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+struct Vector2{
+     float x;
+     float y;
+};
+
 class Game
 {
 private:
-     bool isRunniing;//ゲームがスタートしているかどうか
-     SDL_Window* window;
-public:
-     Game(/* args */);
-     ~Game();
+     bool mIsRunniing; // ゲームがスタートしているかどうか
+     SDL_Window *mWindow;
+     SDL_Renderer *mRenderer;
 
-     int Setup();
+     const int mWidth;//画面幅
+     const int mHeight;//画面の高さ
+
+     const int mThickness;//壁の厚みとボールの直径
+     const int mPaddleLength; //パドルの長さ
+
+     Vector2 mPaddlePos;//跳ね返す板
+     Vector2 mBallPos;//動き回るボール
+
      void Input();
-     int Draw();
-     void Update();
-};
+     void Draw();
 
+     void DrawShape(int,int,int,int);//図形を描画する処理
+
+public:
+     Game();
+     ~Game();
+     bool Initialize();
+     void Update();
+     void Shutdown();
+};
 
 #endif /* GAME_HPP */
--- book_2/SDL_TEST01/src/game.cpp
+++ book_2/SDL_TEST01/src/game.cpp
@@ -1,67 +1,138 @@
 #include "game.hpp"
 
-//コンストラクタ
-Game::Game() : isRunniing(true) {
+// コンストラクタ
+Game::Game() : 
+mIsRunniing(true),
+mWidth(1024),
+mHeight(720),
+mThickness(15),
+mPaddleLength(200),
+mPaddlePos{static_cast<float>(mThickness),static_cast<float>(mHeight/2 - mPaddleLength/2)},
+mBallPos{static_cast<float>(mWidth/2),static_cast<float>(mHeight/2)}
+{
      std::cout << "セットアップ開始" << std::endl;
-     Setup();
 }
 
-//デストラクタ
-Game::~Game(){
+// デストラクタ
+Game::~Game()
+{
      std::cout << "ゲーム終了" << std::endl;
 }
 
-int Game::Setup() {
-    std::cout << "Game setup" << std::endl;
-    // SDLの初期化
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return 1;
-    }
+bool Game::Initialize()
+{
+     std::cout << "Game setup" << std::endl;
 
-    // ウィンドウの作成
-    window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        return 1;
-    }
-    return 0;
+     // SDLの初期化
+     if (SDL_Init(SDL_INIT_VIDEO) < 0)
+     { // 他にも様々な初期化がある Audioとか諸々
+          printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+          return false;
+     }
+
+     // ウィンドウの作成
+     mWindow = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, mWidth, mHeight, SDL_WINDOW_SHOWN);
+     if (mWindow == NULL)
+     { // もし失敗するとnullptrになるらしい
+          printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+          return false;
+     }
+
+     // レンダラーの作成
+     mRenderer = SDL_CreateRenderer(
+         mWindow,                                             // 描画する対象のウィンドウ
+         -1,                                                  // ウィンドウが今回は一つなのでデフォルトの-1
+         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC // 垂直同期を有効にする（バッファを入れ替えるやつ）
+     );
+
+     if (mRenderer == NULL)
+     {
+          return false;
+     }
+
+     return true;
 }
 
-void Game::Input() {
-     //std::cout << "Processing user input" << std::endl;
-}
+void Game::Input()
+{
+     // std::cout << "Processing user input" << std::endl;
+     SDL_Event event;
 
-int Game::Draw() {
-    //std::cout << "Drawing the game" << std::endl;
-
-    SDL_Event e;
-     // イベントの処理
-     while (SDL_PollEvent(&e) != 0) {
-          if (e.type == SDL_QUIT) {
-               isRunniing = false;
+     // キューにイベントがあれば繰り返し
+     // ウィンドウのxボタンとか
+     while (SDL_PollEvent(&event))
+     {
+          switch (event.type)
+          {
+          case SDL_QUIT:
+               mIsRunniing = false;
+               break;
+          default:
+               break;
           }
      }
 
-     // ウィンドウに描画などの処理を追加
-
-     // 一時的に適当なウェイト
-     SDL_Delay(16);
-     return 0;
+     // キーボードの入力を取得する
+     const Uint8 *state = SDL_GetKeyboardState(NULL);
+     if (state[SDL_SCANCODE_ESCAPE])
+     {
+          mIsRunniing = false;
+     }
 }
 
-void Game::Update() {
-    std::cout << "Updating game state" << std::endl;
-    while(Game::isRunniing){
-          //ここにフレームに合わせた処理を書く
+void Game::Draw()
+{
+     // std::cout << "Drawing the game" << std::endl;
+
+     //背景の描画
+     SDL_SetRenderDrawColor(mRenderer,0,0,255,255);
+     SDL_RenderClear(mRenderer);
+     
+     //壁の描画
+     DrawShape(0,0,mWidth,mThickness);//上壁
+     DrawShape(0,mHeight-mThickness,mWidth,mThickness);//下壁
+     //DrawShape(0,0,mThickness,mHeight);//左壁
+     DrawShape(mWidth-mThickness,0,mThickness,mHeight);//右壁
+
+     //ボールの描画
+     DrawShape(static_cast<int>(mBallPos.x),static_cast<int>(mBallPos.y),mThickness,mThickness);
+     
+     //制御板の描画
+     DrawShape(static_cast<int>(mPaddlePos.x),static_cast<int>(mPaddlePos.y),mThickness,mPaddleLength);
+
+     SDL_RenderPresent(mRenderer);
+}
+
+//引数は開始位置x,開始位置y,図形の横幅,図形の縦幅の順番
+void Game::DrawShape(int x ,int y,int width,int height){
+     SDL_SetRenderDrawColor(mRenderer,255,255,255,255);
+     SDL_Rect shape{
+          x,//左上隅x
+          y,//左上隅y
+          width,//図形の横幅
+          height//図形の高さ
+     };
+     SDL_RenderFillRect(mRenderer,&shape);
+}
+
+void Game::Update()
+{
+     std::cout << "Updating game state" << std::endl;
+     while (Game::mIsRunniing)
+     {
+          // ここにフレームに合わせた処理を書く
           Input();
           Draw();
-    }
-
-    // ウィンドウの破棄
-    SDL_DestroyWindow(window);
-
-    // SDLの終了
-    SDL_Quit();
+          SDL_Delay(16);
+     }
 }
 
+void Game::Shutdown()
+{
+     // レンダラーの破棄
+     SDL_DestroyRenderer(mRenderer);
+     // ウィンドウの破棄
+     SDL_DestroyWindow(mWindow);
+     // SDLの終了
+     SDL_Quit();
+}
